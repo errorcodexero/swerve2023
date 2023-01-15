2835435ca9c4055ee4c7e71f53188363ba98fff9
Butch Griffin <butchg@comcast.net>
2023-01-15 12:15:27 -0800

    copied over swerve test modes from swerve 2022

--- src/main/java/frc/robot/automodes/SwerveRobot2023AutoModeController.java
+++ src/main/java/frc/robot/automodes/SwerveRobot2023AutoModeController.java
@@ -1,12 +1,49 @@
 package frc.robot.automodes;
 
+import java.util.List;
+
 import org.xero1425.base.XeroRobot;
 import org.xero1425.base.controllers.AutoController;
+import org.xero1425.base.controllers.AutoMode;
 import org.xero1425.misc.BadParameterTypeException;
+import org.xero1425.misc.MessageLogger;
+import org.xero1425.misc.MessageType;
 import org.xero1425.misc.MissingParameterException;
 
 public class SwerveRobot2023AutoModeController extends AutoController {
-    public SwerveRobot2023AutoModeController(XeroRobot robot) throws MissingParameterException, BadParameterTypeException {
-        super(robot, "SwerveRobot2023AutoModeController") ;
+        private SwerveTestAutoMode test_mode_ ;
+    
+        public SwerveRobot2023AutoModeController(XeroRobot robot) throws MissingParameterException, BadParameterTypeException {
+            super(robot, "SwerveRobot2023AutoModeController");
+    
+            try {
+                test_mode_ = new SwerveTestAutoMode(this) ;
+            }
+            catch(Exception ex) {
+                MessageLogger logger = robot.getMessageLogger() ;
+                logger.startMessage(MessageType.Error).add("Exception thrown creating automodes - ") ;
+                logger.add(ex.getMessage()).endMessage();
+                robot.logStackTrace(ex.getStackTrace());
+            }
+        }
+    
+        public void updateAutoMode(int mode, String gamedata) {
+    
+            AutoMode modeobj = null ;
+    
+            if (isTestMode()) {
+                modeobj = test_mode_ ;
+            }
+            else {
+                List<AutoMode> automodes = getAllAutomodes() ;
+                if (mode >= 0 && mode < automodes.size()) {
+                    modeobj = automodes.get(mode) ;
+                }
+            }
+    
+            if (getAutoMode() != modeobj) {
+                setAutoMode(modeobj) ;
+            }
+        }
     }
-}
+    
---
+++ src/main/java/frc/robot/automodes/SwerveTestAutoMode.java
@@ -0,0 +1,66 @@
+package frc.robot.automodes;
+
+import org.xero1425.base.controllers.TestAutoMode;
+import org.xero1425.base.subsystems.swerve.common.SwerveBaseSubsystem;
+import org.xero1425.base.subsystems.swerve.common.SwerveHolonomicPathFollower;
+import org.xero1425.base.subsystems.swerve.common.SwervePowerAngleAction;
+import org.xero1425.base.subsystems.swerve.common.SwerveSpeedAngleAction;
+
+import frc.robot.subsystems.SwerveRobot2023Subsystem;
+
+public class SwerveTestAutoMode extends TestAutoMode {
+
+    public SwerveTestAutoMode(SwerveRobot2023AutoModeController ctrl) throws Exception {
+        super(ctrl, "Swerver-Test-Mode");
+
+        SwerveRobot2023Subsystem robotsys = (SwerveRobot2023Subsystem) ctrl.getRobot().getRobotSubsystem();
+        SwerveBaseSubsystem swerve = (SwerveBaseSubsystem) robotsys.getDB();
+
+        double[] angles = new double[4];
+        double[] powers = new double[4];
+
+        switch (getTestNumber()) {
+            case 0:
+                // Set the steering motor to the angle given, and the drive motor to the power given.  Run indefintely.  Action will
+                // stop the plot after the default plot interval (four seconds)
+                addSubActionPair(swerve, new SwervePowerAngleAction(swerve, getDouble("angle"), getDouble("power")), true) ;
+                break;
+
+            case 1:
+                // Set the steering motor to the angle given, and the drive motor to the power given.  Run  until the duration has expired
+                addSubActionPair(swerve, new SwervePowerAngleAction(swerve, getDouble("angle"), getDouble("power"), getDouble("duration")), true) ;
+                break ;
+
+            case 2:
+                // Set the steering motor to the angle given, and the drive motor to the speed given.  Run indefintely.  Action will
+                // stop the plot after the default plot interval (four seconds).  Since speed is given, the PID controller will try to
+                // maintain the target speed
+                addSubActionPair(swerve, new SwerveSpeedAngleAction(swerve, getDouble("angle"), getDouble("speed")), true) ;
+                break;
+
+            case 3:
+                // Set the steering motor to the angle given, and the drive motor to the speed given.  Run  until the duration has expired.
+                // Since speed is given, the PID controller will try to maintain the target speed
+                addSubActionPair(swerve, new SwerveSpeedAngleAction(swerve, getDouble("angle"), getDouble("speed"), getDouble("duration")), true) ;
+                break ;                
+
+            case 4:
+                // Run the path follower against the path given
+                addSubActionPair(swerve, new SwerveHolonomicPathFollower(swerve, getString("name"), true), true);
+                break ;
+
+            case 5:
+                // Set the steering motor to the angle given, and the drive motor to the power given.  Run  until the duration has expired
+                angles[0] = getDouble("angle");
+                angles[1] = getDouble("angle");
+                angles[2] = getDouble("angle");
+                angles[3] = getDouble("angle");
+                powers[0] = getDouble("power");
+                powers[1] = getDouble("power");
+                powers[2] = getDouble("power");
+                powers[3] = getDouble("power");
+                addSubActionPair(swerve, new SwervePowerAngleAction(swerve, angles, powers, getDouble("duration")), true) ;
+                break ;
+        }
+    }
+}
